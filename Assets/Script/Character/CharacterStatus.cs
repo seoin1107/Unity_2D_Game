99da5da25d325e01e6dbf2a023af2128ca58b288
CoinVillain17 <seoin1107@naver.com>
2024-11-05 13:23:06 +0900

    Update CharacterStatus.cs

--- Assets/Script/Character/CharacterStatus.cs
+++ Assets/Script/Character/CharacterStatus.cs
@@ -2,40 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterStatus : MonoBehaviour
+
+public class Stat : MonoBehaviour
 {
-    class Stat
-    {
-        public int level;
+    public int level = 1;
 
-        public int baseHp;
-        public int maxHp;
-        public int curHp;
-
-        public int baseAtk;
+    public int baseHp = 20;
+    public int maxHp;
+    public int curHp;
 
 
-        public int statAtk;
-        public int statHp;
-        public int statUtil;
-
-        public float moveSpeed;
-        public float atkSpeed;
-        public float hpRegen;
-
-        public float hitRecover;
-        public float skillCool;
-        public float skillDamage;
-
-        public float drain;
-        public float dodgeTime;
-        public float dodgeCool;
-        public float parryingTime;
-        public float parryingCool;
+    public int baseAtk = 10;
+    public int totalAtk = 0;
 
 
+    public int statAtk = 0;
+    public int statHp = 0;
+    public int statUtil = 0;
 
-    }
+    public float moveSpeed = 1.0f;
+    public float atkSpeed = 0;
+    public float hpRegen = 0;
+
+    public float hitRecover = 0.5f;
+    public float skillCool = 1.0f;
+    public float skillDamage = 1.0f;
+
+    public float drain = 0;
+
+    public float dodgeTime = 0.2f;
+    public float dodgeCool = 5.0f;
+    public float parryingTime = 0.2f;
+    public float parryingCool = 2.0f;
+
+    public int needExp = 10;
+    public int curExp = 0;
+
+
+
+
+}
+
+public class CharacterStatus : Stat
+{
+    
 
     // Start is called before the first frame update
     void Start()
@@ -48,4 +58,10 @@ public class CharacterStatus : MonoBehaviour
     {
 
     }
+
+    public void LevelUp()
+    {
+        
+    }
+
 }
