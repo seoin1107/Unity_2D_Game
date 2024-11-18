2ede9a233fdfe29a0fd895fc227b001164f5cb41
allenceza <50540635+allenceza@users.noreply.github.com>
2024-11-18 09:05:24 +0900

    폰트추가,NPC레이어추가,깔짝수정

--- Assets/Script/NPC/NPCText.cs
+++ Assets/Script/NPC/NPCText.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class NPCText : MonoBehaviour
 {
+    //ø©±‚∞° NPC∏‡∆Æ
     [SerializeField] private TMP_Text textLabel;
 
     void Start()
@@ -13,7 +14,16 @@ public class NPCText : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        //if (Input.GetKeyDown(KeyCode.G))
+        //{
+        //    GetComponent<TypewriterEffect>().Run("Hello, asdfasfa kanskldfn asdfas" +
+        //    "\nadafasdf", textLabel);
+        //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player") && Input.GetKeyDown(KeyCode.G))
         {
             GetComponent<TypewriterEffect>().Run("Hello, asdfasfa kanskldfn asdfas" +
             "\nadafasdf", textLabel);
