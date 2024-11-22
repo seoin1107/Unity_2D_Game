3826f629aa080cabe7a31dfde7dbed406676db10
swpang10 <swpang10@naver.com>
2024-11-22 13:43:54 +0900

    1122스탯2
    불러오기 미완

--- Assets/Script/Move/Player2D.cs
+++ Assets/Script/Move/Player2D.cs
@@ -19,6 +19,8 @@ public class Player2D : BattleSystem2D
     // Start is called before the first frame update
     void Start()
     {
+        playerStatus = this;
+
         playerStatus.characterStat.baseHP = 20;
         playerStatus.characterStat.baseAtk = 10;
 
