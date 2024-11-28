1f01045fb46ac2c34491362461c83d36b1c65ec0
swpang10 <swpang10@naver.com>
2024-11-28 13:05:47 +0900

    세이브 / 시작화면

---
+++ Assets/Script/UI/LoadGameUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+
+public class LoadGameUI : StartUI
+{
+
+    public static LoadGameUI Instance;
+    public Button loadButton1;
+    public Button loadButton2;
+    public Button loadButton3;
+    public CharacterStatus characterStatus;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!File.Exists(filePath+"Save1"))
+        {
+            
+        }
+        Instance = this;
+        gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnLoadSave1()
+    {
+        mySave.characterStat = FileManager.LoadFromJson<Stat>(filePath+"Save.dat");
+    }
+    public void OnLoadSave2()
+    {
+        mySave.characterStat = FileManager.LoadFromJson<Stat>(filePath+"Save.dat");
+    }
+    public void OnLoadSave3()
+    {
+        mySave.characterStat = FileManager.LoadFromJson<Stat>(filePath+"Save.dat");
+    }
+
+
+}
---
+++ Assets/Script/UI/StartUI.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUI : MonoBehaviour
+{
+
+    public CharacterStatus mySave;
+    public string filePath = Application.dataPath + "/Data/Save/"; //세이브 파일 경로
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void OnStartNewGame()
+    {
+        PlayerInitialize();
+
+    }
+
+    public void OnOffLoadGameMenu()
+    {
+        if (LoadGameUI.Instance != null)
+        {
+            LoadGameUI.Instance.gameObject.SetActive(!LoadGameUI.Instance.gameObject.activeSelf);
+        }
+    }
+
+    public void OnOptionMenu()
+    {
+
+    }
+
+    public void OnExitGame()
+    {
+
+    }
+
+
+    public void PlayerInitialize() //플레이어 스텟 초기화
+    {
+        mySave.characterStat.baseHP = 20;
+        mySave.characterStat.baseAtk = 10;
+        mySave.characterStat.moveSpeed = 5.0f;
+        mySave.characterStat.atkSpeed = 0;
+        mySave.characterStat.attackRange = 1.0f;
+        mySave.characterStat.level = 1;
+        mySave.characterStat.hpRegen = 0;
+        mySave.characterStat.totalPoint = 1;
+        mySave.characterStat.atkPoint = 0;
+        mySave.characterStat.hpPoint = 0;
+        mySave.characterStat.utilPoint = 0;
+        mySave.characterStat.hitRecover = 0.5f;
+        mySave.characterStat.skillCool = 1.0f;
+        mySave.characterStat.skillDamage = 1.0f;
+        mySave.characterStat.drain = 0;
+        mySave.characterStat.dodgeTime = 0.2f;
+        mySave.characterStat.dodgeCool = 5.0f;
+        mySave.characterStat.parryingTime = 0.2f;
+        mySave.characterStat.parryingCool = 2.0f;
+        mySave.characterStat.needExp = 10;
+        mySave.characterStat.curExp = 0;
+        mySave.characterStat.eqiupCard = new int[3] { 0, 0, 0 };
+    }
+
+}
