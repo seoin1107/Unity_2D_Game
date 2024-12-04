1e7453952726050440efb253b8e7bf89e26d28e8
DESKTOP-8900FC7\Atents <silvercrowkws@naver.com>
2024-12-04 13:50:28 +0900

    드디어 배경완성?

--- Assets/Script/StudyBG/BackCameraManager.cs
+++ Assets/Script/StudyBG/BackCameraManager.cs
@@ -2,37 +2,34 @@ using UnityEngine;
 
 public class BackCameraManager : MonoBehaviour
 {
-    public Transform player;          // 플레이어 Transform
-    public float scrollSpeedX;
-    public float scrollSpeedY;         // 배경 속도 조정 (0.5 = 플레이어 속도의 50%)
-    public float initialYOffset;      // y축 시작 위치 오프셋
-    public float backgroundWidth;     // 배경 이미지의 가로 길이
-    public float backgroundHeight;    // 배경 이미지의 세로 길이
-    public Camera backgroundCamera;   // 배경 카메라 (일반 카메라)
+    public Transform playerTransform;  // 플레이어의 Transform
+    public Transform backgroundCamera;  // 배경 카메라의 Transform
 
-    private Camera cam;               // 배경 카메라 컴포넌트
-    private float startPositionX;     // 초기 x축 위치
+    public float xMovementRatio = 0.5f;  // 배경 카메라 x축 이동 비율 (플레이어 이동량에 대한 비율)
+    public float yMovementRatio = 0.2f;  // 배경 카메라 y축 이동 비율 (플레이어 이동량에 대한 비율)
+    public float yOffset = 0f;  // 배경 카메라의 y축 오프셋 (플레이어의 y축을 따라가되 약간 차이를 둘 때 사용)
 
-    private void Start()
+    private Vector3 previousPlayerPosition;  // 이전 플레이어의 위치 (이동량을 계산하기 위함)
+
+    void Start()
     {
-        cam = GetComponent<Camera>(); // 배경 카메라 컴포넌트 가져오기
-        startPositionX = transform.position.x; // 초기 x 위치 저장
-        // 배경 카메라의 y축 시작 위치 설정
-        transform.position = new Vector3(transform.position.x, player.position.y + initialYOffset, transform.position.z);
+        previousPlayerPosition = playerTransform.position;
     }
 
-    private void LateUpdate()
+    void Update()
     {
-        // x축: 무한 스크롤 방식으로 배경 이미지 이동
-        float targetX = player.position.x * scrollSpeedX;
+        // x축: 플레이어의 이동량에 비례한 배경 카메라의 x축 이동
+        float playerXMovement = playerTransform.position.x - previousPlayerPosition.x;
+        float targetX = backgroundCamera.position.x + playerXMovement * xMovementRatio;
 
-        // y축: 플레이어의 y축 위치를 기준으로 배경 카메라 이동 (필요에 따라 감쇠 효과를 줄 수도 있음)
-        float targetY = player.position.y * scrollSpeedY + initialYOffset;
+        // y축: 플레이어의 y축 이동량에 대한 비율로 배경 카메라가 이동하도록 설정
+        float playerYMovement = playerTransform.position.y;
+        float targetY = playerYMovement * yMovementRatio + yOffset;
 
-        // x축 범위 제한 (배경 이미지의 크기에 맞춰서 무한 스크롤)
-        float clampedX = Mathf.Repeat(targetX - startPositionX, backgroundWidth) + startPositionX;
+        // 배경 카메라의 위치를 설정
+        backgroundCamera.position = new Vector3(targetX, targetY, backgroundCamera.position.z);
 
-        // 배경 카메라 위치 업데이트 (y축은 플레이어의 y위치에 따라 움직이고, x축은 무한 스크롤)
-        backgroundCamera.transform.position = new Vector3(clampedX, targetY, backgroundCamera.transform.position.z);
+        // 이전 위치 업데이트 (다음 업데이트에서 이동량을 계산할 수 있도록)
+        previousPlayerPosition = playerTransform.position;
     }
 }
--- Assets/Script/StudyBG/Backgroundmanager.cs
+++ Assets/Script/StudyBG/Backgroundmanager.cs
@@ -2,100 +2,47 @@ using UnityEngine;
 
 public class Backgroundmanager : MonoBehaviour
 {
-    public ObjectPool_sub objectPool; // 오브젝트 풀
-    public float backgroundWidth = 21.504f; // 배경 이미지의 가로 크기
-    public Transform player; // 플레이어 위치
-    public int initialBackgroundCount = 3; // 초기 배경 개수
-    public float yPosition = 1.2f; // 원하는 Y값을 설정
+    public Transform player;             // 플레이어 Transform
+    public float scrollSpeed;            // 배경의 스크롤 속도
+    public float backgroundWidth;        // 배경 이미지의 가로 크기
+    public GameObject backgroundPrefab;  // 배경 이미지 프리팹
+    public Camera backgroundCamera;      // 배경 카메라
 
-    private BackgroundNode head;  // 링크드 리스트의 첫 번째 배경
-    private BackgroundNode tail;  // 링크드 리스트의 마지막 배경
+    private GameObject[] backgroundObjects; // 배경 이미지 객체 배열
+    private int currentBackgroundIndex = 0; // 현재 배경 인덱스
 
-    private void Start()
+    void Start()
     {
-        // 초기 배경 설정
-        for (int i = 0; i < initialBackgroundCount; i++)
+        // 배경 이미지를 복제하여 초기화
+        backgroundObjects = new GameObject[2]; // 배경 2개를 반복해서 사용
+
+        for (int i = 0; i < backgroundObjects.Length; i++)
         {
-            SpawnBackground(i * backgroundWidth); // 배경 배치
+            backgroundObjects[i] = Instantiate(backgroundPrefab);
+            backgroundObjects[i].transform.position = new Vector3(i * backgroundWidth, 0, 0); // 배경을 가로로 배치
         }
     }
 
-    private void Update()
+    void Update()
     {
-        // 배경을 양쪽으로 스크롤하기 위해 플레이어의 위치에 따라 배경을 이동
-        if (player.position.x > tail.background.transform.position.x)
-        {
-            MoveBackgroundToRight();
-        }
-        else if (player.position.x < head.background.transform.position.x)
-        {
-            MoveBackgroundToLeft();
-        }
+        MoveBackground();
     }
 
-    private void SpawnBackground(float xPosition)
+    private void MoveBackground()
     {
-        GameObject background = objectPool.GetObject();
+        // 배경의 위치를 이동시켜 무한 스크롤 처리
+        float targetX = player.position.x * scrollSpeed;
+        backgroundObjects[currentBackgroundIndex].transform.position = new Vector3(targetX, 0, backgroundObjects[currentBackgroundIndex].transform.position.z);
 
-
-        background.transform.position = new Vector3(xPosition, yPosition, 0); // x와 y 위치 설정
-
-        // 새 노드를 링크드 리스트에 추가
-        BackgroundNode newNode = new BackgroundNode { background = background };
-        if (head == null)
+        // 배경이 끝까지 지나가면 위치를 리셋하여 반복
+        if (backgroundObjects[currentBackgroundIndex].transform.position.x <= -backgroundWidth)
         {
-            head = tail = newNode; // 첫 번째 배경
+            // 배경이 화면을 벗어났을 경우 위치를 초기화하고 다음 배경으로
+            backgroundObjects[currentBackgroundIndex].transform.position = new Vector3(backgroundObjects[(currentBackgroundIndex + 1) % 2].transform.position.x + backgroundWidth, 0, 0);
+            currentBackgroundIndex = (currentBackgroundIndex + 1) % 2; // 배경 인덱스 변경
         }
-        else
-        {
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
-        }
-    }
 
-    private void MoveBackgroundToRight()
-    {
-        // 링크드 리스트에서 head 배경을 제거하고 tail로 이동
-        BackgroundNode oldHead = head;
-        head = head.next;
-        head.prev = null;
-
-        // 배경을 오른쪽 끝으로 재배치
-        float newXPosition = tail.background.transform.position.x + backgroundWidth;
-        float newYPosition = tail.background.transform.position.y;
-        oldHead.background.transform.position = new Vector3(newXPosition, newYPosition, 0);
-
-        // tail에 추가
-        tail.next = oldHead;
-        oldHead.prev = tail;
-        tail = oldHead;
-        tail.next = null;
-    }
-
-    private void MoveBackgroundToLeft()
-    {
-        // 링크드 리스트에서 tail 배경을 제거하고 head로 이동
-        BackgroundNode oldTail = tail;
-        tail = tail.prev;
-        tail.next = null;
-
-        // 배경을 왼쪽 끝으로 재배치
-        float newXPosition = head.background.transform.position.x - backgroundWidth;
-        float newYPosition = tail.background.transform.position.y;
-        oldTail.background.transform.position = new Vector3(newXPosition, newYPosition, 0);
-
-        // head에 추가
-        oldTail.next = head;
-        head.prev = oldTail;
-        head = oldTail;
-    }
-
-    // 배경을 위한 노드 클래스
-    private class BackgroundNode
-    {
-        public GameObject background; // 배경 오브젝트
-        public BackgroundNode next;   // 다음 노드
-        public BackgroundNode prev;   // 이전 노드
+        // 배경 카메라는 y축만 따라가고, x축은 무한 스크롤 처리
+        backgroundCamera.transform.position = new Vector3(player.position.x * scrollSpeed, backgroundCamera.transform.position.y, backgroundCamera.transform.position.z);
     }
 }
