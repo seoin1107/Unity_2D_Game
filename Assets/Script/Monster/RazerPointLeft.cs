80e1b5bd409acbc206aebb7dd9fe5833798d2e21
jms <seoin1107@naver.com>
2024-12-16 13:46:46 +0900

    123

--- Assets/Script/Monster/RazerPointLeft.cs
+++ Assets/Script/Monster/RazerPointLeft.cs
@@ -8,16 +8,30 @@ public class RazerPointLeft : BattleSystem2D
 
     public GameObject orgRazer;
     public Transform myRazerPoint;
+
+    public int minRazerCount; // 최소 소환 개수
+    public int maxRazerCount; // 최대 소환 개수
+    public float spawnDelay; // 레이저 소환 간의 딜레이
     // Start is called before the first frame update
     void Start()
     {
-        OnAttack();
+        StartCoroutine(RandomSpawnRazers()); // 랜덤 소환 코루틴 시작
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator RandomSpawnRazers()
+    {
+        int razerCount = Random.Range(minRazerCount, maxRazerCount + 1); // 랜덤 소환 개수 결정
+
+        for (int i = 0; i < razerCount; i++)
+        {
+            OnAttack(); // 레이저 소환
+            yield return new WaitForSeconds(spawnDelay); // 딜레이 후 다음 소환
+        }
     }
     public void OnAttack()
     {
