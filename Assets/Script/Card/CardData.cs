ea2802d5837b917d750d6626f3f7546df999bee1
rvf67 <rvf67@naver.com>
2024-12-24 13:49:17 +0900

    카드이동완료/스텟적용중

--- Assets/Script/Card/CardData.cs
+++ Assets/Script/Card/CardData.cs
@@ -6,24 +6,24 @@ using UnityEngine;
 
 
 
-[CreateAssetMenu(fileName = "CardData", menuName = "/CardData", order = 1)]
+[CreateAssetMenu(fileName = "CardData", menuName = "Card/CardData", order = 1)]
 
 public class CardData : ScriptableObject
 {
     public Sprite icon;
     //카드 정보
-    protected int cardNo;
-    protected int cardRarity;
+    public int cardNo;
+    public int cardRarity;
     //카드 효과
-    protected  float hp_per;         //체력%
-    protected float atk_per;        //공격력%
-    protected float hp_add;         //체력+
-    protected float atk_add;        //공격력+
-    protected float hp_re;          //체력 재생
-    protected float atk_ab;         //공격시 흡혈
-    protected float hit_re;         //피격무적
-    protected float skill_dm;       //스킬 데미지
-    protected float skill_cool;      //스킬 쿨타임
+    public float hp_per;         //체력%
+    public float atk_per;        //공격력%
+    public float hp_add;         //체력+
+    public float atk_add;        //공격력+
+    public float hp_re;          //체력 재생
+    public float atk_ab;         //공격시 흡혈
+    public float hit_re;         //피격무적
+    public float skill_dm;       //스킬 데미지
+    public float skill_cool;      //스킬 쿨타임
 
 
 }
