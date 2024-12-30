d46b8c14b992797f859290475942e26c5fffce78
rvf67 <rvf67@naver.com>
2024-12-30 13:50:44 +0900

    ASD

---
+++ Assets/Script/Card/EquipCard.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipCard : MonoBehaviour
+{
+
+    public Transform equipCard1;
+    public Transform equipCard2;
+    public Transform equipCard3;
+    public DragCard dragCard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckEquipeCard()
+    {
+        if (dragCard = equipCard1.GetComponentInChildren<DragCard>())
+        {
+          
+        }
+    }
+}
